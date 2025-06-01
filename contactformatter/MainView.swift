dbfabdb47e659a03144545a5628b54ca1fb507df
Faraaz Sareshwala <faraazs@gmail.com>
2025-06-01 13:52:18 -0700

    MainView: extract restricted and denied views into their own structs

--- contactformatter/MainView.swift
+++ contactformatter/MainView.swift
@@ -2,9 +2,30 @@ import Contacts
 import PhoneNumberKit
 import SwiftUI
 
-struct MainView: View {
-  @State var authorizationStatus = CNContactStore.authorizationStatus(for: .contacts)
+struct DeniedView: View {
+  let deniedMessage = """
+    This device has been denied access to your contacts. Please update your settings to allow \
+    access.
+    """
 
+  var body: some View {
+    ActionView(
+      message: deniedMessage,
+      button: "Open Settings",
+      action: {
+        openSettings()
+      }
+    )
+  }
+
+  func openSettings() {
+    if let url = URL(string: UIApplication.openSettingsURLString) {
+      UIApplication.shared.open(url)
+    }
+  }
+}
+
+struct RestrictedView: View {
   let restrictedMessage = """
     This application is not authorized to access contact data.
 
@@ -12,41 +33,26 @@ struct MainView: View {
     parental controls being in place.
     """
 
-  let deniedMessage = """
-    This device has been denied access to your contacts. Please update your settings to allow \
-    access.
-    """
+  var body: some View {
+    MessageView(message: restrictedMessage)
+  }
+}
+
+struct MainView: View {
+  @State var authorizationStatus = CNContactStore.authorizationStatus(for: .contacts)
 
   var body: some View {
     switch authorizationStatus {
     case .authorized, .limited:
       ContactListView()
     case .notDetermined:
-        WelcomeView(authorizationStatus: $authorizationStatus)
+      WelcomeView(authorizationStatus: $authorizationStatus)
     case .restricted:
-      MessageView(message: restrictedMessage)
+      RestrictedView()
     case .denied:
-      ActionView(
-        message: deniedMessage,
-        button: "Open Settings",
-        action: {
-          openSettings()
-        }
-      )
+      DeniedView()
     @unknown default:
-      ActionView(
-        message: deniedMessage,
-        button: "Open Settings",
-        action: {
-          openSettings()
-        }
-      )
-    }
-  }
-
-  func openSettings() {
-    if let url = URL(string: UIApplication.openSettingsURLString) {
-      UIApplication.shared.open(url)
+      DeniedView()
     }
   }
 }
