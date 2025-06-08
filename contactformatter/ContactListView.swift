0ae850e0475f5d6119aec935c45439d4c6277b97
Faraaz Sareshwala <faraazs@gmail.com>
2025-06-08 11:12:33 -0700

    ContactListVIew: move ContactView to ContactListView

--- contactformatter/ContactListView.swift
+++ contactformatter/ContactListView.swift
@@ -16,6 +16,34 @@ struct Contact: Identifiable {
   }
 }
 
+struct ContactView: View {
+  @Binding var isChecked: Bool
+  let name: String
+  let phoneNumber: String
+
+  var body: some View {
+    HStack {
+      Button(action: {
+        isChecked.toggle()
+      }) {
+        Image(systemName: isChecked ? "checkmark.circle.fill" : "circle")
+          .foregroundColor(isChecked ? .blue : .gray)
+          .padding(.trailing)
+      }
+
+      Text(name)
+      Spacer()
+      Text(phoneNumber).font(.footnote)
+    }
+    .onAppear {
+      isChecked = true
+    }
+    .onDisappear {
+      isChecked = false
+    }
+  }
+}
+
 struct ContactListView: View {
   @State private var contacts: [Contact] = []
   @State private var selectedFormatType: PhoneNumberFormat = .international
